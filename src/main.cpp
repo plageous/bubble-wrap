1a10e51027a7195e8b4cca3f0ee502b6d54b53eb
KJ <122484032+plageous@users.noreply.github.com>
2026-01-15 01:26:44 +0000

    Added core functionally to main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 //"KJ" Miclea
 // Bubble Wrap
 // Began on 1/14
+#include <bn_core.h>
 #include <bn_backdrop.h>
 #include <bn_color.h>
 
 int main()
 {
-
+    bn::core::init();
     bn::backdrop::set_color(bn::color(20, 20, 31));
 }
