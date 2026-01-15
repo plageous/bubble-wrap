bfe83977bc6218497b16f52773538942b2292e3e
KJ <122484032+plageous@users.noreply.github.com>
2026-01-15 01:31:01 +0000

    Added functionality to core functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,15 @@
 // Bubble Wrap
 // Began on 1/14
 #include <bn_core.h>
-#include <bn_backdrop.h>
 #include <bn_color.h>
+#include <bn_backdrop.h>
 
 int main()
 {
     bn::core::init();
     bn::backdrop::set_color(bn::color(20, 20, 31));
+    
+    while(true) {
+        bn::core::update();    
+    }
 }
